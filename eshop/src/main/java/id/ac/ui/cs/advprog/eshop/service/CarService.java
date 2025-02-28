3bfdc89d81d0be58d2ed3650fcdc0b34d793491b
MichelineWL <michelinewijaya@gmail.com>
2025-02-28 11:37:56 +0700

    feat: car service implementation

--- eshop/src/main/java/id/ac/ui/cs/advprog/eshop/service/CarService.java
+++ eshop/src/main/java/id/ac/ui/cs/advprog/eshop/service/CarService.java
@@ -2,8 +2,8 @@ package id.ac.ui.cs.advprog.eshop.service;
 import id.ac.ui.cs.advprog.eshop.model.Car;
 import java.util.List;
 
-public class CarService {
-    public Car createCar(Car car);
+public interface CarService {
+    public Car create(Car car);
     public List<Car> findAll();
     Car findById(String carId);
     public void update(String carId, Car car);
