3ad1185ac97805b2ed0dff6a29a9a5d61fe968f6
MichelineWL <michelinewijaya@gmail.com>
2025-02-28 18:34:26 +0700

    refactor: SRP implementation

---
+++ eshop/src/main/java/id/ac/ui/cs/advprog/eshop/service/CarQueryServiceImpl.java
@@ -0,0 +1,28 @@
+package id.ac.ui.cs.advprog.eshop.service;
+
+import id.ac.ui.cs.advprog.eshop.model.Car;
+import id.ac.ui.cs.advprog.eshop.repository.CarRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+@Service
+public class CarQueryServiceImpl implements CarQueryService {
+    @Autowired
+    private CarRepository carRepository;
+
+    @Override
+    public List<Car> findAll() {
+        Iterator<Car> carIterator = (Iterator<Car>) carRepository.findAll();
+        List<Car> allCar = new ArrayList<>();
+        carIterator.forEachRemaining(allCar::add);
+        return allCar;
+    }
+
+    @Override
+    public Car findById(String carId) {
+        return carRepository.findById(carId);
+    }
+}
