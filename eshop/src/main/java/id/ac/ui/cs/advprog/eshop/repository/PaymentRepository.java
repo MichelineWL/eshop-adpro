84be9ed851d9f62268e76f26bfc5a1cde009f3c8
MichelineWL <michelinewijaya@gmail.com>
2025-03-07 13:17:16 +0700

    feat: [RED] Add PaymentRepository skeleton

---
+++ eshop/src/main/java/id/ac/ui/cs/advprog/eshop/repository/PaymentRepository.java
@@ -0,0 +1,17 @@
+package id.ac.ui.cs.advprog.eshop.repository;
+
+import id.ac.ui.cs.advprog.eshop.model.Payment;
+import java.util.ArrayList;
+import java.util.List;
+
+public class PaymentRepository {
+    private List<Payment> payments = new ArrayList<>();
+
+    public void save(Payment payment) {
+        return;
+    }
+
+    public List<Payment> findAll() {
+        return null;
+    }
+}
--- eshop/src/test/java/id/ac/ui/cs/advprog/eshop/repository/PaymentRepositoryTest.java
+++ eshop/src/test/java/id/ac/ui/cs/advprog/eshop/repository/PaymentRepositoryTest.java
@@ -10,11 +10,16 @@ import static org.junit.jupiter.api.Assertions.*;
 public class PaymentRepositoryTest {
     private PaymentRepository repository;
     private Order order;
+    private List<Product> products;
 
     @BeforeEach
     void setUp() {
+        this.products = new ArrayList<>();
+        Product product1 = new Product("10", "Sampo Cap Bambang", 2);
+        this.products.add(product1);
+
         repository = new PaymentRepository();
-        order = new Order("101", 1700000000L, "Diana Putri");
+        order = new Order("101", this.products, 1700000000L, "Diana Putri");
     }
 
     @Test
