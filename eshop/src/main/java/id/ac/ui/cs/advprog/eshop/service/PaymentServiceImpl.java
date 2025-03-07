a0b2caf5500189ccaef33ca3f142f90e8c58e28a
MichelineWL <michelinewijaya@gmail.com>
2025-03-07 13:43:45 +0700

    feat: [RED] Add PaymentServiceImpl skeleton

---
+++ eshop/src/main/java/id/ac/ui/cs/advprog/eshop/service/PaymentServiceImpl.java
@@ -0,0 +1,32 @@
+package id.ac.ui.cs.advprog.eshop.service;
+
+import id.ac.ui.cs.advprog.eshop.model.Payment;
+
+import java.util.List;
+
+public class PaymentServiceImpl implements PaymentService {
+    @Override
+    public void save(Payment payment) {
+        throw new UnsupportedOperationException("Not implemented yet");
+    }
+
+    @Override
+    public Payment findById(String id) {
+        throw new UnsupportedOperationException("Not implemented yet");
+    }
+
+    @Override
+    public List<Payment> findAll() {
+        throw new UnsupportedOperationException("Not implemented yet");
+    }
+
+    @Override
+    public void delete(String id) {
+        throw new UnsupportedOperationException("Not implemented yet");
+    }
+
+    @Override
+    public void updateStatus(String id, String status) {
+        throw new UnsupportedOperationException("Not implemented yet");
+    }
+}
